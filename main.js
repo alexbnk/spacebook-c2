850cf94522a7b3eea5a2a11bc8492c00ae0dc57c
alexbnk <alexbnk@gmail.com>
2017-02-17 00:59:26 +0200

    fixed post owner and time design

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ var users = [{
     userid: 3,
     userFullName: "Sarah Connor"
 }]
+
+var postOwner = users[0].userFullName;
+
+
 var postTime = function() {
     return new Date();
 }
@@ -17,27 +21,22 @@ var postTime = function() {
 // generate random ID function
 function randomID(length) {
     var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
-
     if (!length) {
         length = Math.floor(Math.random() * chars.length);
     }
-
     var str = '';
     for (var i = 0; i < length; i++) {
         str += chars[Math.floor(Math.random() * chars.length)];
     }
     return str;
 }
-
-// ---------------------------- Functions
-
 // Empty the post input field after post
 var clearInput = function() {
     $('#post-name').val("");
+    console.log($('#post-name').val("")+"- cleared the post input box");
 };
 
-
-// remove post
+// Remove post
 var removePost = function(deleteThis) {
     //$thispost = $(this).parents('.posts').find('.post');
     //  console.log(deleteThis);
@@ -49,10 +48,9 @@ var removePost = function(deleteThis) {
     }
 };
 
-
-// post element HTML creation
-var postelm = function(id, text, time) {
-    var p1 = "<div class=\"thumbnail post\" data-id=\"" + id + "\"><h3>" + text + "</h3><span class=\"time\">" + time + "</span><img src=\"https://unsplash.it/g/640/200\" >";
+// Post element HTML creation
+var postelm = function(id, text, time, user,picurl) {
+    var p1 = "<div class=\"thumbnail post\" data-id=\""+id+"\"><div class=\"postowner\">"+user+"</div><span class=\"time\">"+time+"</span><div class=\"posttext\">"+text+"</div><img src=\"https://unsplash.it/640/200\" >";
     var p2 = " <div class=\"caption\"><span class=\"glyphicon glyphicon-remove-circle deletePostIcon\"></span>";
     var p3 = "<span class=\"time\">" + time + "</span>";
     var p4 = "";
@@ -63,20 +61,19 @@ var postelm = function(id, text, time) {
     return posthtml;
 }
 
-//  create post function
-
+//  Create post function that inserts new posts to array posts
 var newPost = function(post) {
     postID = randomID(8);
     posts.push({
         text: post,
         id: postID,
-        time: Date()
+        time: Date(),
+        user: postOwner
     });
-
+    console.log("post "+this.postID+" pushed to array")
 };
 
-// post the post on post click :)
-
+// Post the post on post click :)
 $('.add-post').on('click', function() {
 
     if ($('#post-name').val().length > 0) {
@@ -86,23 +83,18 @@ $('.add-post').on('click', function() {
         console.log("Posted");
     }
 });
-
-
-
-
 // Show the posts on the page
-
 var drawPosts = function() {
     //remove previously posted posts;
     $(".posts").find('.post').remove();
     //loop the posts array and prepend the obects
     for (var i = 0; i < posts.length; i++) {
-        $(".posts").prepend(postelm(posts[i].id, posts[i].text), posts[i].time);
+        $(".posts").prepend(postelm(posts[i].id, posts[i].text, posts[i].time,postOwner));
         //$('.posts').on('click','.post', function(){});
     }
 }
 
-
+// Delete post from screen and posts array
 $('.posts').on('click', '.deletePostIcon', function() {
     var postToDelete = $(this).closest('.post')
     console.log(postToDelete);
